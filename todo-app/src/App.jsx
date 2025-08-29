8806947adc40bd84e2fa77906c7e5903eace1357
DavidMEa <mea.david29@outlook.com>
2025-08-29 18:33:33 -0400

    adding  task done and managing dynamic values

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -11,21 +11,44 @@ import { TodoCard } from './components/TodoCard'
 import { TodoInput } from './components/TodoInput'
 import { TodoList } from './components/TodoList'
 
+
 function App() {
 
-  const todos = [
-    { input: 'Hello! Add your first todo!', complete: true },
-    { input: 'Get the groceries!', complete: false },
-    { input: 'Learn how to web design', complete: false },
-    { input: 'Say hi to gran gran', complete: true },
-  ]
+  // const todos = [
+  //   { input: 'Hello! Add your first todo!', complete: true },
+  //   { input: 'Get the groceries!', complete: false },
+  //   { input: 'Learn how to web design', complete: false },
+  //   { input: 'Say hi to gran gran', complete: true },
+  // ]
+
+  const [todos, setTodos] = useState([
+    { input: 'Hello! Add your first todo!', complete: true }
+  ])
+
+  const [selectedTab, setSelectedTab] = useState('Open')
+
+  function handleAddTodo(newTodo) {
+    const newTodoList = [...todos, { input: newTodo, complete: false }]
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo() {
+
+  }
+
+  function handleDeleteTodo() {
+
+  }
   return (
     <>
-      <Header todos={todos}/>
-      <Tabs  todos={todos}/>
-     
-      <TodoInput  todos={todos}/>
-      <TodoList  todos={todos}/>
+      <Header todos={todos} />
+      <Tabs 
+        todos={todos} // The array of todo items, used to display counts or filter todos by tab
+        selectedTab={selectedTab} // The currently selected tab (e.g., 'Open', 'Completed', etc.)
+        setSelectedTab={setSelectedTab} // Function to update the selected tab when the user clicks a tab
+      /> 
+      <TodoInput todos={todos} handleAddTodo={handleAddTodo} />
+      <TodoList todos={todos} />
     </>
   )
 
